4c36e51acec308befdf8e55597dc2ac3f97e6b24
Fahad Mehboob <fahad.fakky@hotmail.com>
2025-06-30 20:11:49 +0500

    animation and and onboarding package included

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class ImageString {
+  ImageString._();
+
+  static const String onBoardingImage1 = 'assets/images/logo.png';
+  static const String onBoardingImage2 = 'assets/images/splash.png';
+  static const String onBoardingImage3 = 'assets/images/cricket.png';
+}
